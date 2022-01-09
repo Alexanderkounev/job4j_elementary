5ed06a2d3a968bbe347e5f00f455c127e4dcefe8
Коунев Александр <97198065+Alexanderkounev@users.noreply.github.com>
2022-01-09 19:40:08 +0300

    Delete StudentInfo.java

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Alexander Kounev");
-        System.out.println("09.10.1990");
-    }
-}
