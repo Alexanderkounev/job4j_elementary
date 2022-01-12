c50e43df568125338046fd393dfee4a670938d42
SKYDEFREAM <skydream855@gmail.com>
2022-01-12 20:39:31 +0300

    SqArea

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -3,11 +3,13 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double rsl = p / (2 * (k + 1));
-        return rsl;
+        double l = rsl * k;
+        double s = l * rsl;
+        return s;
     }
 
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
 }
