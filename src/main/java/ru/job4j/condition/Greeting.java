7e0460efbf924a886c16ac6b417c7f2161eca440
SKYDEFREAM <skydream855@gmail.com>
2022-01-12 20:26:38 +0300

    Greeting

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,8 +2,9 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
+        String idea = "I like Java! ";
+        System.out.println(idea + "But I am a newbie.");
         int year = 2022;
-        System.out.println(idea + " But I am a newbie. " + year);
+        System.out.println(idea + year);
            }
 }
