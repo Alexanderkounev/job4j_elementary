f29b53cf185273d2a495f7d865350dc98fd7a813
SKYDEFREAM <skydream855@gmail.com>
2022-01-14 14:57:23 +0300

    PointTest

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double expected = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when34to56then2() {
+        int x1 = 3;
+        int y1 = 4;
+        int x2 = 5;
+        int y2 = 6;
+        double expected = 2.82f;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when111333to222666then2() {
+        int x1 = 111;
+        int y1 = 333;
+        int x2 = 222;
+        int y2 = 666;
+        double expected = 351.01f;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
