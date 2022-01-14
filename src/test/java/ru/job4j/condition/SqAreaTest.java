de51d844eb97754d40f9ad04c07f5554cbdeb604
SKYDEFREAM <skydream855@gmail.com>
2022-01-14 15:10:34 +0300

    SqAreaTest

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP7K3Square229() {
+        float expected = 2.29f;
+        int p = 7;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP15K7Square615() {
+        float expected = 6.15f;
+        int p = 15;
+        double k = 7;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP34K13Square2() {
+        float expected = 19.16f;
+        int p = 34;
+        double k = 13;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
