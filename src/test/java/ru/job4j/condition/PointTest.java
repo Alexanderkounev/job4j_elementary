95c04e9a1d959e94739f0b58d82615a7716a34cd
SKYDEFREAM <skydream855@gmail.com>
2022-01-16 11:19:35 +0300

    PointTest

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -17,23 +17,23 @@ public class PointTest {
     }
 
     @Test
-    public void when34to56then2() {
+    public void when34to56then2dot82() {
         int x1 = 3;
         int y1 = 4;
         int x2 = 5;
         int y2 = 6;
-        double expected = 2.82f;
+        double expected = 2.82;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when111333to222666then2() {
+    public void when111333to222666then351dot01() {
         int x1 = 111;
         int y1 = 333;
         int x2 = 222;
         int y2 = 666;
-        double expected = 351.01f;
+        double expected = 351.01;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
