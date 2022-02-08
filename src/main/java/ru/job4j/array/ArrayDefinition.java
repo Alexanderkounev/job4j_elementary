ce90fca0ab6531284ef4bb0e1e3c8e0ae58cfea0
SKYDEFREAM <skydream855@gmail.com>
2022-02-08 21:57:21 +0300

    ArrayNames

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива будет равен: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива будет равен: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива будет равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Chuck Norris";
+        names[1] = "Jean-Claude Van Damme";
+        names[2] = "Arnold Schwarzenegger";
+        names[3] = "Sylvester v Stolovoi";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
