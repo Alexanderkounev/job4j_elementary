3a85072665a54cb07f8daef380b55ae30e9e3528
SKYDEFREAM <skydream855@gmail.com>
2022-03-16 15:51:26 +0300

    40205

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] strings = new String[4];
-        strings[0] = "Chuck Norris";
-        strings[1] = "Jean-Claude Van Damme";
-        strings[2] = "Arnold Schwarzenegger";
-        strings[3] = "Sylvester v Stolovoi";
-        System.out.println(strings[0]);
-        System.out.println(strings[1]);
-        System.out.println(strings[2]);
-        System.out.println(strings[3]);
+        String[] names = new String[4];
+        names[0] = "Chuck Norris";
+        names[1] = "Jean-Claude Van Damme";
+        names[2] = "Arnold Schwarzenegger";
+        names[3] = "Sylvester v Stolovoi";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
