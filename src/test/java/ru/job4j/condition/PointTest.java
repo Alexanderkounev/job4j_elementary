bee074a7ab0f0a313a6a9ce7036d6397e69d8f1e
SKYDEFREAM <skydream855@gmail.com>
2022-03-28 16:54:06 +0300

    Point + Test

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,35 +7,29 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when34to56then2dot82() {
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 5;
-        int y2 = 6;
+        Point a = new Point(3, 4);
+        Point b = new Point(5, 6);
+        double dist = a.distance(b);
         double expected = 2.82;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when111333to222666then351dot01() {
-        int x1 = 111;
-        int y1 = 333;
-        int x2 = 222;
-        int y2 = 666;
+        Point a = new Point(111, 333);
+        Point b = new Point(222, 666);
+        double dist = a.distance(b);
         double expected = 351.01;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
 
     }
 }
